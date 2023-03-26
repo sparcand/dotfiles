623b4bbc1cee4d5cc0044c8b1b3376d0c11ce0f3
Steve Arcand <steve@stevearcand.com>
2023-03-26 08:30:42 -0600

    Remove oh-my-zsh dir, fix editor, update keybindings

--- dot_config/nvim/lua/sparcand/core/options.lua
+++ dot_config/nvim/lua/sparcand/core/options.lua
@@ -1,5 +1,36 @@
 local opt = vim.opt -- for conciceness
 
+-- :help options
+opt.backup = false -- creates a backup file
+opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
+opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+opt.conceallevel = 0 -- so that `` is visible in markdown files
+opt.fileencoding = "utf-8" -- the encoding written to a file
+opt.hlsearch = true -- highlight all matches on previous search pattern
+opt.ignorecase = true -- ignore case in search patterns
+opt.mouse = "a" -- allow the mouse to be used in neovim
+opt.pumheight = 10 -- pop up menu height
+opt.showtabline = 2 -- always show tabs
+opt.smartcase = true -- smart case
+opt.smartindent = true -- make indenting smarter again
+opt.splitbelow = true -- force all horizontal splits to go below current window
+opt.splitright = true -- force all vertical splits to go to the right of current window
+opt.swapfile = false -- creates a swapfile
+opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
+opt.undofile = true -- enable persistent undo
+opt.updatetime = 300 -- faster completion (4000ms default)
+opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+opt.numberwidth = 4 -- set number column width to 2 {default 4}
+opt.scrolloff = 8 -- is one of my fav
+opt.sidescrolloff = 8
+opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+
+opt.shortmess:append("c")
+
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd([[set iskeyword+=-]])
+vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
+
 -- Line numbers
 opt.relativenumber = true
 opt.number = true
@@ -18,7 +49,7 @@ opt.ignorecase = true
 opt.smartcase = true
 
 -- cursor line
-opt.cursorline = false
+opt.cursorline = true
 
 -- appearance
 opt.termguicolors = true
@@ -30,10 +61,3 @@ opt.backspace = "indent,eol,start"
 
 -- clipboard
 opt.clipboard:append("unnamedplus")
-
--- split windows
-opt.splitright = true
-opt.splitbelow = true
-
--- Consider dash part of word
-opt.iskeyword:append("-")
